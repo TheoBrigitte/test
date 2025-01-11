fc2f31dd73e65e04f25155020ceecf8b4ed00f1f
Theo Brigitte <theo.brigitte@gmail.com>
2025-01-11 20:56:39 +0100

    add action commands

--- src/github-action.js
+++ src/github-action.js
@@ -1,16 +1,42 @@
 import core from '@actions/core';
 
-import { fmt } from './fmt.js';
+import { cmd } from './main.js';
 
+const command = core.getInput('command');
 const options = {
   path: core.getInput('path'),
   format: core.getInput('format'),
   encoding: core.getInput('encoding')
 };
 
+const args = [
+  ...process.argv.slice(0, 2),
+  command,
+  '--path',
+  options.path,
+  '--format',
+  options.format,
+  '--encoding',
+  options.encoding
+];
+
+let successMessage;
+switch (command) {
+  case 'fmt':
+    successMessage = 'Changelog is valid';
+    break;
+  case 'release':
+    const version = core.getInput('release_version');
+    args.push(version);
+    successMessage = `Released version ${version}`;
+    break;
+  default:
+    throw new Error(`Unsupported command: ${command}`);
+}
+
 try {
-  fmt(options);
-  console.log('Changelog is valid');
+  cmd().parse(args);
+  console.log(successMessage);
 } catch (error) {
   core.setFailed(error.message);
 }
