0054c08eb5fece5e20f3e91298e814346c3bb3e8
Theo Brigitte <theo.brigitte@gmail.com>
2025-01-12 16:16:10 +0100

    use custom email

--- src/github-action.js
+++ src/github-action.js
@@ -74,8 +74,8 @@ async function pushChanges(options) {
     // Changes to commit
   }
 
-  await exec.exec('git', ['config', 'user.name', 'github-actions[bot]']);
-  await exec.exec('git', ['config', 'user.email', 'action@github.com']);
+  await exec.exec('git', ['config', 'user.name', 'changelog-manager[bot]']);
+  await exec.exec('git', ['config', 'user.email', 'changelog-manager.bot@users.noreply.github.com']);
   await exec.exec('git', ['commit', '-m', 'Update changelog', options.path]);
   await exec.exec('git', ['push', 'origin', `HEAD:${github.context.payload.pull_request.head.ref}`]);
 
