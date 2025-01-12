abedb0dc40bb8d865daba46767a746d3581f8276
Theo Brigitte <theo.brigitte@gmail.com>
2025-01-12 16:01:57 +0100

    use github-actions

--- src/github-action.js
+++ src/github-action.js
@@ -59,8 +59,8 @@ async function pushChanges(options) {
     // Changes to commit
   }
 
-  await exec.exec('git', ['config', '--global', 'user.name', 'changelog-manager']);
-  await exec.exec('git', ['config', '--global', 'user.email', 'changelog-manager@users.noreply.github.com']);
+  await exec.exec('git', ['config', 'user.name', 'github-actions[bot]']);
+  await exec.exec('git', ['config', 'user.email', 'action@github.com']);
   await exec.exec('git', ['commit', '-m', 'Update changelog', options.path]);
   await exec.exec('git', ['push', 'origin', `HEAD:${github.context.payload.pull_request.head.ref}`]);
   console.log('Pushed changes');
